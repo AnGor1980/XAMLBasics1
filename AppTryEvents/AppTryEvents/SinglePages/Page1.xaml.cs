9c4e98248c04e23484f2d03a44091578e7ce8311
Gorshkov <ag@vollers.com>
2018-04-26 16:31:00 +0200

    Add project files.

---
+++ AppTryEvents/AppTryEvents/SinglePages/Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace AppTryEvents.SinglePages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+        int i;
+
+        public Page1 ()
+		{
+
+            InitializeComponent ();
+            buttonNavigatePage2.Text = "Navigate!";
+            buttonNavigatePage2.Clicked += ButtonNavigatePage2_Clicked;
+
+            button1.Text = "Click!";
+            button1.HorizontalOptions = LayoutOptions.Center;
+            button1.VerticalOptions = LayoutOptions.Center;
+            button1.Clicked += Button1_Clicked;
+        }
+
+        private void ButtonNavigatePage2_Clicked(object sender, EventArgs e)
+        {
+
+            //Navigation.PushAsync(new Page2());
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            i++;
+            label1.Text = System.String.Format("Clicked {0} times", i);
+        }
+
+	}
+}
